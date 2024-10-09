5bc5b005a8ed1de76c85ebe74c12084f3abd9c69
AntonyHaro <antonyhpro@outlook.com>
2024-10-09 19:32:05 -0300

    adição de um arquivo de teste para melhor depuração

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import xmlrpc.client
 import csv
 import os
+import time
 from dotenv import load_dotenv
 from get_ids import get_country_id, get_state_id, get_existing_contacts
 
@@ -29,8 +30,11 @@ def import_csv_contacts(file_name):
 
             # get the contact info from the csv file
             for row_index, row in enumerate(reader, start=1):
-                contact_name = (row.get("name") or "").strip()
-                contact_email = (row.get("email") or "").strip()
+                # contact_name = (row.get("name") or "").strip()
+                # contact_email = (row.get("email") or "").strip()
+                
+                contact_name = (row.get("Nome") or "").strip()
+                contact_email = (row.get("E-mail") or "").strip()
 
                 # check if the name or email is alredy in the sets
                 if contact_name in seen_names or contact_email in seen_emails:
@@ -40,31 +44,59 @@ def import_csv_contacts(file_name):
                 seen_names.add(contact_name)
                 seen_emails.add(contact_email)
 
+                # contact = {
+                #     # default res.partner fields
+                #     "name": contact_name,
+                #     "email": contact_email,
+                #     "function": (row.get("function") or "").strip(),
+                #     "company_name": (row.get("company_name") or "").strip(),
+                #     "city": (row.get("city") or "").strip(),
+                #     "country_id": (row.get("country_id") or "").strip(),
+                #     "state_id": (row.get("state_id") or "").strip(),
+                #     "street": (row.get("street") or "").strip(),
+                    
+                #     # custom fields
+                #     "x_linkedin": (row.get("x_linkedin") or "").strip(),
+                #     "x_redes_sociais": (row.get("x_redes_sociais") or "").strip(),
+                #     "x_setor": (row.get("x_setor") or "").strip(),
+                    
+                #     # custom text field for the company info
+                #     "x_info_empresa": f"""
+                #         Nome: {(row.get("company_name") or "").strip()}
+                #         Localização: {(row.get("local_empresa") or "").strip()}
+                #         Telefone da sede: {(row.get("telefone_sede") or "").strip()}
+                #         Redes Sociais: {(row.get("redes_sociais_empresa") or "").strip()}
+                #         Setor: {(row.get("setor_empresa") or "").strip()}
+                #         Tamanho: {(row.get("tamanho_empresa") or "").strip()}
+                #         URL: {(row.get("url_empresa") or "").strip()}
+                #     """
+                # }
+                
                 contact = {
                     # default res.partner fields
                     "name": contact_name,
                     "email": contact_email,
-                    "function": (row.get("function") or "").strip(),
-                    "company_name": (row.get("company_name") or "").strip(),
-                    "city": (row.get("city") or "").strip(),
-                    "country_id": (row.get("country_id") or "").strip(),
-                    "state_id": (row.get("state_id") or "").strip(),
-                    "street": (row.get("street") or "").strip(),
+                    "function": (row.get("Cargo") or "").strip(),
+                    "company_name": (row.get("Nome da empresa") or "").strip(),
+                    "city": (row.get("Cidade") or "").strip(),
+                    "country_id": (row.get("País") or "").strip(),
+                    "state_id": (row.get("Estado") or "").strip(),
+                    "street": (row.get("Localização") or "").strip(),
                     
                     # custom fields
-                    "x_linkedin": (row.get("x_linkedin") or "").strip(),
-                    "x_redes_sociais": (row.get("x_redes_sociais") or "").strip(),
-                    "x_setor": (row.get("x_setor") or "").strip(),
+                    "x_linkedin": (row.get("LinkedIn") or "").strip(),
+                    "x_redes_sociais": (row.get("Usuário - redes sociais") or "").strip(),
+                    "x_setor": (row.get("Setor") or "").strip(),
                     
                     # custom text field for the company info
                     "x_info_empresa": f"""
-                        Nome: {(row.get("company_name") or "").strip()}
-                        Localização: {(row.get("local_empresa") or "").strip()}
-                        Telefone da sede: {(row.get("telefone_sede") or "").strip()}
-                        Redes Sociais: {(row.get("redes_sociais_empresa") or "").strip()}
-                        Setor: {(row.get("setor_empresa") or "").strip()}
-                        Tamanho: {(row.get("tamanho_empresa") or "").strip()}
-                        URL: {(row.get("url_empresa") or "").strip()}
+                        Nome: {(row.get("Nome da empresa") or "").strip()}
+                        Localização: {(row.get("Localização da empresa") or "").strip()}
+                        Telefone da sede: {(row.get("Telefone da sede") or "").strip()}
+                        Redes Sociais: {(row.get("Empresa - redes sociais") or "").strip()}
+                        Setor: {(row.get("Setor da empresa") or "").strip()}
+                        Tamanho: {(row.get("Tamanho da empresa") or "").strip()}
+                        URL: {(row.get("URL da empresa") or "").strip()}
                     """
                 }
 
@@ -125,11 +157,9 @@ def create_contacts(url, db, uid, password, contacts):
             
             if country_id:
                 contact["country_id"] = country_id
-                print(contact["country_id"])
 
             if state_id:
                 contact["state_id"] = state_id
-                print(contact["state_id"])
                 
             # create the contact using the res.partner model and print the contact_id
             contact_id = models.execute_kw(db, uid, password, "res.partner", "create", [contact])
@@ -149,7 +179,7 @@ def main():
     uid = authenticate(odoo_url, odoo_db, odoo_username, odoo_password)
     if uid:
         # get the contacts from csv
-        contacts = import_csv_contacts("file.csv")
+        contacts = import_csv_contacts("test.csv")
         
         if contacts:
             print(f"\nTotal de contatos para serem carregados: {len(contacts)}\n")
@@ -157,5 +187,12 @@ def main():
             # create contacts from the array of contacts
             create_contacts(odoo_url, odoo_db, uid, odoo_password, contacts)
     
+# if __name__ == "__main__":
+#     main()
+
 if __name__ == "__main__":
+    start_time = time.time()  # Registra o tempo antes de chamar main
     main()
+    end_time = time.time()  # Registra o tempo após a execução de main
+    elapsed_time = end_time - start_time  # Calcula o tempo decorrido
+    print(f"Tempo de execução: {elapsed_time:.2f} segundos")  # Exibe o tempo decorrido
